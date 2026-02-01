5ef1b47454b1e6981f1c9d5f993db478a7e32934
NishantsCode <work.nishantraj@gmail.com>
2026-02-02 03:40:05 +0530

    Updated the cors issue

--- Backend/utils/jwt.js
+++ Backend/utils/jwt.js
@@ -7,8 +7,8 @@ const generateToken = (userId, res) => {
     res.cookie("jwt", token, {
         maxAge: 30*24*60*60*1000,
         httpOnly: true,
-        sameSite: "strict",
-        secure: process.env.NODE_ENV !== "development"
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+        secure: process.env.NODE_ENV === "production"
     })
 }
 
